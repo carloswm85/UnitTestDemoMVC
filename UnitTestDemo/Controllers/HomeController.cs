b08a32b971662067bed58ead6aedfa7e5d45e681
Carlos W. Mercado <carloswashingtonmercado@gmail.com>
2022-10-17 14:06:47 -0300

    Small fix.

--- UnitTestDemo/Controllers/HomeController.cs
+++ UnitTestDemo/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace UnitTestDemo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
         //public HomeController(ILogger<HomeController> logger)
         //{
